c9feafb9a0d717967991b8039767ddeced9a995d
DINUSHA NIWAN <119559999+Code-With-Dinuwa@users.noreply.github.com>
2024-08-30 12:52:57 +0530

    Update admin-login.php

--- php/admin-login.php
+++ php/admin-login.php
@@ -3,10 +3,10 @@
 session_start();
 
 // Database credentials
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "akila_sir";
+$servername = "sql12.freesqldatabase.com";
+$username = "sql12728588";
+$password = "wbrhtUq1KD";
+$dbname = "sql12728588";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
