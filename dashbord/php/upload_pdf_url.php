a693f0bc57089edb247b62b22fbd821825bdd99b
DINUSHA NIWAN <119559999+Code-With-Dinuwa@users.noreply.github.com>
2024-08-30 13:13:19 +0530

    Update upload_pdf_url.php

--- dashbord/php/upload_pdf_url.php
+++ dashbord/php/upload_pdf_url.php
@@ -1,9 +1,10 @@
 <?php
 
-$servername = "sql12.freesqldatabase.com";
+$servername = "sql12.freesqldatabase.com:3306";
 $username = "sql12728588";
 $password = "wbrhtUq1KD";
 $dbname = "sql12728588";
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
