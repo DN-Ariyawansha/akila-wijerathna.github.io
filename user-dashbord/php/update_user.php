aff5d79d7c4a82fb7f5030cfd636784d8fa29802
DINUSHA NIWAN <119559999+Code-With-Dinuwa@users.noreply.github.com>
2024-08-30 13:14:27 +0530

    Update update_user.php

--- user-dashbord/php/update_user.php
+++ user-dashbord/php/update_user.php
@@ -1,10 +1,11 @@
 <?php
 session_start(); // Start the session
 
-$servername = "sql12.freesqldatabase.com";
+$servername = "sql12.freesqldatabase.com:3306";
 $username = "sql12728588";
 $password = "wbrhtUq1KD";
 $dbname = "sql12728588";
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
