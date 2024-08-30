8d27b1a0a48316ec6374070c118b05cab06cd34a
DINUSHA NIWAN <119559999+Code-With-Dinuwa@users.noreply.github.com>
2024-08-30 13:14:01 +0530

    Update fetch_user_details.php

--- user-dashbord/php/fetch_user_details.php
+++ user-dashbord/php/fetch_user_details.php
@@ -3,10 +3,11 @@ session_start(); // Ensure session is started to access session variables
 
 
 
-$servername = "sql12.freesqldatabase.com";
+$servername = "sql12.freesqldatabase.com:3306";
 $username = "sql12728588";
 $password = "wbrhtUq1KD";
 $dbname = "sql12728588";
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
