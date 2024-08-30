50e902e66a0478de53abfe16f6e550f645e12262
DINUSHA NIWAN <119559999+Code-With-Dinuwa@users.noreply.github.com>
2024-08-30 13:09:30 +0530

    Update fetch_lecture_details.php

--- php/fetch_lecture_details.php
+++ php/fetch_lecture_details.php
@@ -2,10 +2,11 @@
 // fetch_lecture_details.php
 
 // Database connection details
-$servername = "sql12.freesqldatabase.com";
+$servername = "sql12.freesqldatabase.com:3306";
 $username = "sql12728588";
 $password = "wbrhtUq1KD";
 $dbname = "sql12728588";
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
