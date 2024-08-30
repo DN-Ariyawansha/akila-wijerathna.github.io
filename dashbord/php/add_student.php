e1f28de867da153f4f79aad77ce5bb68bf8bdbf4
DINUSHA NIWAN <119559999+Code-With-Dinuwa@users.noreply.github.com>
2024-08-30 13:11:30 +0530

    Update add_student.php

--- dashbord/php/add_student.php
+++ dashbord/php/add_student.php
@@ -1,9 +1,10 @@
 <?php
 // Database connection
-$servername = "sql12.freesqldatabase.com";
+$servername = "sql12.freesqldatabase.com:3306";
 $username = "sql12728588";
 $password = "wbrhtUq1KD";
 $dbname = "sql12728588";
+
 
 $conn = new mysqli($host, $user, $pass, $db);
 
